4dbc767e432f9cba21b894ae91d2664d61fecd0d
Angry Fennec <angryfennecus@gmail.com>
2018-08-09 07:59:58 +0300

    Экраны в случайном порядке

---
+++ js/main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const mainElement = document.querySelector(`#main`);
+
+const selectSlide = (element) => {
+  mainElement.innerHTML = ``;
+  mainElement.appendChild(element.cloneNode(true));
+};
+
+const screens = Array.from(document.querySelectorAll(`template`)).
+  map((it) => it.content);
+
+let current = 0;
+const select = (index) => {
+  index = index < 0 ? screens.length - 1 : index;
+  index = index >= screens.length ? 0 : index;
+  current = index;
+  selectSlide(screens[current]);
+};
+
+document.addEventListener(`keydown`, (evt) => {
+  switch (evt.keyCode) {
+    case 37:
+      select(current + 1);
+      break;
+    case 39:
+      select(current - 1);
+      break;
+  }
+});
+
+select(0);

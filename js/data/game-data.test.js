dc6ea533d97e8bb695163209218ca277fb9376e7
Angry Fennec <angryfennecus@gmail.com>
2018-08-26 13:07:50 +0300

    Тесты и остальные функции

--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -1,5 +1,5 @@
 import {assert} from 'chai';
-import {countScore, addAnswer, getRandomInt} from '../game-data.js';
+import {countScore, addAnswer, getRandomInt, changeLevel, changeLives, INITIAL_GAME, changeTime} from '../game-data.js';
 
 describe(`Check score count`, () => {
 
@@ -43,4 +43,64 @@ describe(`Check score count`, () => {
 
 });
 
-//  mocha.run();
+describe(`Check level changer`, () => {
+  it(`should update level of the game`, () => {
+    assert.equal(changeLevel(INITIAL_GAME, 1).level, 1);
+    assert.equal(changeLevel(INITIAL_GAME, 2).level, 2);
+    assert.equal(changeLevel(INITIAL_GAME, 10).level, 10);
+  });
+
+  it(`should not allow set negative values`, () => {
+    assert.throws(() => changeLevel(INITIAL_GAME, -1).level, /Level should not be negative value/);
+  });
+  it(`should be less than LEVEL_LIMIT`, () => {
+    assert.throws(() => changeLevel(INITIAL_GAME, 102).level, /Level should not be greater than LEVEL_LIMIT/);
+  });
+
+  it(`should not allow set non number value`, () => {
+    assert.throws(() => changeLevel(INITIAL_GAME, []).level, /Level should be of type number/);
+  });
+
+});
+
+describe(`Check lives changer`, () => {
+
+  it(`should reduce lives count`, () => {
+    assert.equal(changeLives(INITIAL_GAME, 1).lives, 0);
+    assert.equal(changeLives(INITIAL_GAME, 2).lives, 1);
+    assert.equal(changeLives(INITIAL_GAME, 3).lives, 2);
+  });
+
+  it(`should not allow set negative values`, () => {
+    assert.throws(() => changeLives(INITIAL_GAME, -1).lives, /Lives should not be negative value/);
+  });
+
+  it(`should not allow set non number value`, () => {
+    assert.throws(() => changeLives(INITIAL_GAME, []).lives, /Lives should be of type number/);
+  });
+
+  it(`should be less than LIVES`, () => {
+    assert.throws(() => changeLives(INITIAL_GAME, 4).lives, /Lives should not be greater than LIVES/);
+  });
+});
+
+describe(`Check time changer`, () => {
+
+  it(`should reduce time count`, () => {
+    assert.equal(changeTime(INITIAL_GAME, 0).time, 1);
+    assert.equal(changeTime(INITIAL_GAME, 20).time, 21);
+    assert.equal(changeTime(INITIAL_GAME, 13).time, 14);
+  });
+
+  it(`should not allow set negative values`, () => {
+    assert.throws(() => changeTime(INITIAL_GAME, -1).time, /Time should not be negative value/);
+  });
+
+  it(`should not allow set non number value`, () => {
+    assert.throws(() => changeTime(INITIAL_GAME, []).time, /Time should be of type number/);
+  });
+
+  it(`should be less than TIME_LIMIT`, () => {
+    assert.throws(() => changeTime(INITIAL_GAME, 31).time, /Time should not be greater than TIME_LIMIT/);
+  });
+});
--- js/game-data.js
+++ js/game-data.js
@@ -1,5 +1,12 @@
 const NUM = 10;
 const TIME_LIMIT = 30;
+const LEVEL_LIMIT = 10;
+const LIVES = 3;
+export const INITIAL_GAME = Object.freeze({
+  level: 0,
+  lives: 3,
+  time: 0
+});
 
 export const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -43,4 +50,59 @@ export const countScore = (answers, livesNumber) => {
   }
   score += livesNumber * 50;
   return score;
+};
+
+export const changeLevel = (game, level) => {
+  if (typeof level !== `number`) {
+    throw new Error(`Level should be of type number`);
+  }
+
+  if (level < 0) {
+    throw new Error(`Level should not be negative value`);
+  }
+  if (level > LEVEL_LIMIT) {
+    throw new Error(`Level should not be greater than LEVEL_LIMIT`);
+  }
+  const newGame = Object.assign({}, game, {
+    level
+  });
+  return newGame;
+};
+
+export const changeLives = (game, lives) => {
+  if (typeof lives !== `number`) {
+    throw new Error(`Lives should be of type number`);
+  }
+  if (lives < 0) {
+    throw new Error(`Lives should not be negative value`);
+  }
+  if (lives > LIVES) {
+    throw new Error(`Lives should not be greater than LIVES`);
+  }
+  if (lives > 0 && lives <= LIVES) {
+    lives--;
+  }
+  const newGame = Object.assign({}, game, {
+    lives
+  });
+  return newGame;
+};
+
+export const changeTime = (game, time) => {
+  if (typeof time !== `number`) {
+    throw new Error(`Time should be of type number`);
+  }
+  if (time < 0) {
+    throw new Error(`Time should not be negative value`);
+  }
+  if (time > TIME_LIMIT) {
+    throw new Error(`Time should not be greater than TIME_LIMIT`);
+  }
+  if (time >= 0 && time < TIME_LIMIT) {
+    time++;
+  }
+  const newGame = Object.assign({}, game, {
+    time
+  });
+  return newGame;
 };
